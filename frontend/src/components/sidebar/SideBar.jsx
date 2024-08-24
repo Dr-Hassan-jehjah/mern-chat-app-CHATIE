76784298fd6bed69d7c176a4a69a5ff49e91f768
Dr-Hassan-jehjah <hassanjehjahaz@gmail.com>
2024-08-24 17:14:47 +0300

    2nd part completed - ui design

---
+++ frontend/src/components/sidebar/SideBar.jsx
@@ -0,0 +1,14 @@
+import SearchInput from "./SearchInput";
+import Conversations from "./Conversations";
+const SideBar = () => {
+  return (
+    <div className="border-r border-slate-500 p-4 flex flex-col">
+        <SearchInput></SearchInput>
+        <div className="divider px-3"></div>
+        <Conversations></Conversations>
+        {/* <LogoutButton></LogoutButton> */} */
+    </div>
+  )
+}
+
+export default SideBar
