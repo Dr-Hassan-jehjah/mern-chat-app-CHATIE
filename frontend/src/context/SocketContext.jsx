24123545967d76455a47542714c15f9da2d1bd0a
Dr-Hassan-jehjah <hassanjehjahaz@gmail.com>
2024-08-29 00:38:35 +0300

    several changes

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chatie.onrender.com", {
+			const socket = io("https://chatie-5v3e.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
 		"react/prop-types": "off",
 	},
 	server: {
-		port: 5000,
+		port: 3000,
 		proxy: {
 			"/api": {
 				target: "http://localhost:5000",
